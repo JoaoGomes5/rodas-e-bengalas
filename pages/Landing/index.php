4610206da943528e9fa953079bf4a90c5f55fd93
Joao Gomes <joaopfg.2002@gmail.com>
2022-01-06 00:43:24 +0000

    Finish Landing / LoginOptions / AdminLogin / HomeAdmin / TecLogin / ClientLogin pages

---
+++ pages/Landing/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="icon" type="image/x-icon" href="../../assets/images/favicon.png">
+
+
+  <title>Rodas e Bengalas</title>
+</head>
+<body>
+  <div class="header">
+    <div >
+      <img class="icon" src="../../assets/images/icon.png" alt="Rodas e Bengalas">
+    </div>
+
+    <div>
+        <p>Portugal</p>
+    </div>
+  </div>
+
+  <main>
+    <div class="container">
+      <h1>Venha ser feliz <br> connosco</h1>
+      
+
+
+      
+        <a href="../LoginOptions">Entrar na plataforma</a>
+      
+    </div>
+
+      <img src="../../assets/images/doctor-landing.png" alt="Doctor">
+  </main>
+</body>
+</html>
---
+++ pages/LoginOptions/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="icon" type="image/x-icon" href="../../assets/images/favicon.png">
+
+
+  <title>Rodas e Bengalas</title>
+</head>
+<body>
+  
+  <div class="container">
+    <h1>Como se identifica?</h1>
+    
+      <div class="options-container">
+
+        <div class="admin-option">
+          <a href="../AdminLogin">
+            <img src="../../assets/images/admin-option.jpg" alt="">
+
+            <p>Sou um administrador</p>
+          </a>
+        </div>
+
+        <div class="home-admin-option">
+          <a href="../HomeAdminLogin">
+            <img src="../../assets/images/admin-home-option.jpg" alt="">
+
+            <p>Sou administrador de lar</p>
+          </a>
+        </div>
+
+        <div class="tec-option">
+          <a href="../TechnicianLogin">
+            <img src="../../assets/images/tec-option.jpg" alt="">
+
+            <p>Sou um técnico</p>
+          </a>
+        </div>
+
+        <div class="client-option">
+          <a href="../ClientLogin">
+            <img src="../../assets/images/client-option.jpg" alt="">
+
+            <p>Sou um cliente</p>
+          </a>
+        </div>
+
+      </div>
+    </body>
+  </div>
+</html>
