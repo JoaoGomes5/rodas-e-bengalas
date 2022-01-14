ccd73ba1b11375988b1eb14d3eca59b96202e098
Joao Brito <1200363@isep.ipp.pt>
2022-01-14 22:04:52 +0000

    Add CRUD to Home Class

--- src/home/Home.php
+++ src/home/Home.php
@@ -18,6 +18,19 @@ class Home {
 
     public function set_district($district) {
         $this->district = $district;
+        $id = $this->get_id();
+        $id_district = $district->get_id();
+        $conn = get_connection();
+
+        $sql = "UPDATE homes SET idDistrict = $id_district WHERE id = $id";
+
+        if ($conn->query($sql) === TRUE) {
+            $conn->close();
+            return true;
+        } else {
+            $conn->close();
+            return false;
+        }
     }
 
     public function get_district() {
@@ -38,6 +51,106 @@ class Home {
 
     public function get_description() {
         return $this->description;
+    }
+
+    public function update($district, $address, $description) {
+        $id = $this->get_id();
+        $id_district = $district->get_id();
+        $conn = get_connection();
+
+        $sql = "UPDATE homes SET idDistrict = $id_district, address = $address, description = $description WHERE id = $id";
+
+        if ($conn->query($sql) === TRUE) {
+            $conn->close();
+            return true;
+        } else {
+            $conn->close();
+            return false;
+        }
+    }
+
+    public static function create($district, $address, $description){
+        $id_district = $district->get_id();
+        $conn = get_connection();
+
+        $sql = "INSERT INTO homes (idDistrict, address, description) VALUES ('$id_district', '$address', '$description')";
+
+        if ($conn->query($sql) === TRUE) {
+            $conn->close();
+
+            $home = new Home();
+            $home = $home->get_last_inserted();
+
+            return $home;
+        } else {
+            $conn->close();
+            return false;
+        }
+    }
+
+    public function delete()  {
+        $conn = get_connection();
+        $id = $this->get_id();
+
+        $sql = "DELETE * FROM homes WHERE id = $id";
+
+        if ($conn->query($sql) === TRUE) {
+            $conn->close();
+            return true;
+        } else {
+            $conn->close();
+            return false;
+        }
+    }
+
+    public static function get_by_id(int $id) {
+        $conn = get_connection();
+
+        $sql = "SELECT * FROM homes WHERE id = $id";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $conn->close();
+
+            $home = new Home();
+            $home = $home->row_to_object($row);
+
+            return $home;
+        } else {
+            $conn->close();
+            return null;
+        }
+    }
+
+    private function get_last_inserted(){
+        $conn = get_connection();
+
+        $sql = "SELECT * FROM homes ORDER BY id DESC LIMIT 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $conn->close();
+
+            $home = $this->row_to_object($row);
+
+            return $home;
+        } else {
+            $conn->close();
+            return null;
+        }
+    }
+
+    private function row_to_object($home_row) {
+        $home = new Home();
+        $district = District::get_by_id($home_row["idDistrict"]);
+
+        $home->set_id($home_row["id"]);
+        $home->set_district($district);
+        $home->set_description($home_row["description"]);
+
+        return $home;
     }
 
     public function to_string(){
