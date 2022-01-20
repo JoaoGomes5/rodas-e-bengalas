bc4bfb1d547f58e14ad2616da6b975d15988a478
Joao Brito <1200363@isep.ipp.pt>
2022-01-20 00:19:17 +0000

    Add import option via csv to Users

--- src/csv/import_csv.php
+++ src/csv/import_csv.php
@@ -1,7 +1,7 @@
 <?php
     require_once(dirname(__FILE__, 3) . "/src/database/connection.php");
 
-    function import_csv(){
+    function import_csv_medicine(){
         /*
             Assumindo que a primeira linha é de dados e que os dados estão da seguinte forma:
                 "
@@ -31,6 +31,57 @@
                     $intake = $line[4];
 
                     $sql = "INSERT INTO medicine (quantity, activeIngredient, name, brand, intake) VALUES ('$quantity', '$activeIngredient', '$name', '$brand', '$intake')";
+                    
+                    if ($conn->query($sql) === TRUE) {
+                        $conn->close();
+                        fclose($file);
+                        return true;
+                    } else {
+                        $conn->close();
+                        fclose($file);
+                        return false;
+                    }
+                }
+            }else{
+                return false;
+            }
+        }else{
+            return false; // ficheiro inválido
+        }
+    }
+
+    function import_csv_users(){
+        /*
+            Assumindo que a primeira linha é de dados e que os dados estão da seguinte forma:
+                "
+                    email1, password1, type1, firstName1, lastName1, photo1, idHome1
+                    email2, password2, type2, firstName2, lastName2, photo2, idHome2
+                "
+        */
+        $file_types = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
+        
+        if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $file_types)){
+            
+            if(is_uploaded_file($_FILES['file']['tmp_name'])){
+                
+                $file = fopen($_FILES['file']['tmp_name'], 'r');
+
+                $conn = get_connection();
+                
+                // Saltar a primeira linha
+                // fgetcsv($file);
+                
+                while(($line = fgetcsv($file)) !== FALSE){
+                    // Get row data
+                    $email   = $line[0];
+                    $password  = $line[1];
+                    $type  = $line[2];
+                    $firstName = $line[3];
+                    $lastName = $line[4];
+                    $photo = $line[5];
+                    $idHome = $line[6];
+
+                    $sql = "INSERT INTO users (email, password, type, firstName, lastName, photo, idHome) VALUES ('$email', '$password', '$type', '$firstName', '$lastName', '$photo', '$idHome')";
                     
                     if ($conn->query($sql) === TRUE) {
                         $conn->close();
